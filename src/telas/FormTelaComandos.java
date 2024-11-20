01c559fb84760eac1f636163bbd6aad673ff627d
Rodrigo Slongo <rodrigo.slongotur@hotmail.com>
2024-11-20 18:33:07 -0300

    tela comandos

--- src/telas/FormTelaComandos.java
+++ src/telas/FormTelaComandos.java
@@ -14,25 +14,50 @@ public class FormTelaComandos implements ActionListener {
 	private JPanel painel;
 	private ACMEAirDrones app;
 	private TelaComandos tela;
+	private boolean executado;
 	public FormTelaComandos(TelaComandos tela,ACMEAirDrones app) {
 		this.app = app;
 		this.tela = tela;
+		executado = false;
 		executarButton.addActionListener(this);
 		voltarButton.addActionListener(this);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == executarButton) {
-			String comando = textField1.getText();
-			if (comando.equalsIgnoreCase("SIMULA")) {
-				app.armazenaConteudo();
-			}
-			if (comando.equalsIgnoreCase("FINAL")) {
+		try{
+			if (e.getSource() == executarButton) {
+				String comando = textField1.getText();
 
-			}
+				if (comando.equalsIgnoreCase("SIMULA")) {
+					if(executado){
+						JOptionPane.showMessageDialog(painel,
+								"Comando ja executado",
+								"Aviso",
+								JOptionPane.WARNING_MESSAGE);
+					}else{
+						//TODO CARREGAR O ARQUIVO SIMULA
+						textArea1.setText("Carregando comando...!");
+						executado = true;
+					}
+				}
+				else if (comando.equalsIgnoreCase("FINAL")) {
+					app.armazenaConteudo();
+					textArea1.setText("Arquivo salvo com sucesso!");
+				}else{
+					JOptionPane.showMessageDialog(painel,
+						"Comando inválido",
+							"Erro",
+							JOptionPane.ERROR_MESSAGE);
+				}
 
-		}else if(e.getSource() == voltarButton){
-			tela.setVisible(false);
+			}else if(e.getSource() == voltarButton){
+				tela.setVisible(false);
+			}
+		}catch(Exception ex){
+			JOptionPane.showMessageDialog(painel,
+					ex.getMessage(),
+					"Erro",
+					JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
